b480c70ed9b4faaa2c6c6c7f2d68644091b4fee9
RavenPillmann <rpillmann12@breakthroughsaintpaul.org>
2019-08-17 15:53:35 -0500

    DFS and BFS for BST

--- cpp/BinarySearchTrees.cpp
+++ cpp/BinarySearchTrees.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-#include <vector>
+#include <deque>
 using namespace std;
 
 class Node {
 	public:
-		int value;
+		long value;
 		Node* left;
 		Node* right;
+		bool is_visited;
 
-		Node(int _value) {
+		Node(long _value) {
 			value = _value;
 			left = NULL;
 			right = NULL;
+			is_visited = false;
 		}
 };
 
@@ -20,13 +22,13 @@ class BinarySearchTree {
 	private:
 		Node* root;
 
-		Node* insert_node(Node* _node, int _value) {
+		Node* insert_node(Node* _node, long _value) {
 			if (_node == NULL) {
 				Node* new_node = new Node(_value);
 				return new_node;
 			}
 
-			int value = _node->value;
+			long value = _node->value;
 
 			if (_value < value) {
 				_node->left = insert_node(_node->left, _value);
@@ -46,7 +48,7 @@ class BinarySearchTree {
 			return _node;
 		}
 
-		Node* remove_node(Node* _node, int _value) {
+		Node* remove_node(Node* _node, long _value) {
 			
 			// Base Case 1: No node found
 			if (_node == NULL) {
@@ -85,7 +87,7 @@ class BinarySearchTree {
 				return _node;		
 			}
 
-			int value = _node->value;
+			long value = _node->value;
 
 			if (_value < value) {
 				_node->left = remove_node(_node->left, _value);
@@ -94,6 +96,14 @@ class BinarySearchTree {
 			}
 
 			return _node;
+		}
+
+		void set_all_nodes_unvisited(Node* node) {
+			if (node != NULL) {
+				set_all_nodes_unvisited(node->left);
+				node->is_visited = false;
+				set_all_nodes_unvisited(node->right);
+			}
 		}
 
 		void in_order_traversal(Node* node) {
@@ -120,14 +130,57 @@ class BinarySearchTree {
 			}
 		}
 
+		void dfs(Node* node) {
+			// Check if we're no longer on the tree
+			if (node == NULL) {
+				return;
+			}
+
+			// Visit the node. In this case, visiting is just printing the value.
+			cout << node->value << " ";
+
+			// Keeping track of nodes we visit allows us to skip those nodes, which means we won't be in a loop
+			node->is_visited = true;
+
+			if (node->left != NULL && !node->left->is_visited) dfs(node->left);
+			if (node->right != NULL && !node->right->is_visited) dfs(node->right);
+		}
+
+		void bfs(Node* node) {
+			deque<Node *> to_visit;
+			to_visit.push_back(node);
+
+			Node* visiting;
+			while (!to_visit.empty()) {
+				visiting = to_visit.front();
+				to_visit.pop_front();
+
+				cout << visiting->value << " ";
+
+				visiting->is_visited = true;
+				if (visiting->left && !visiting->left->is_visited) to_visit.push_back(visiting->left);
+				if (visiting->right && !visiting->right->is_visited) to_visit.push_back(visiting->right);
+			}
+		}
+
 	public:
 		BinarySearchTree(Node& _root) {
 			root = &_root;
 		}
 
-		Node* insert(int _value) {
+		Node* insert(long _value) {
 			Node* new_node = insert_node(root, _value);
 			return new_node;
+		}
+
+		Node* remove(long _value) {
+			root = remove_node(root, _value);
+			return root;
+		}
+
+		void unvisit_all_nodes() {
+			cout << "Unvisiting all nodes" << endl;
+			set_all_nodes_unvisited(root);
 		}
 
 		void visit_in_order() {
@@ -148,34 +201,56 @@ class BinarySearchTree {
 			cout << endl;
 		}
 
-		Node* remove(int _value) {
-			root = remove_node(root, _value);
-			return root;
+		void depth_first_search() {
+			cout << "Depth First Search" << endl;
+			dfs(root);
+			cout << endl;
+		}
+
+		void breadth_first_search() {
+			cout << "Breadth First Search" << endl;
+			bfs(root);
+			cout << endl;
 		}
 };
 
 
 int main() {
-	Node node_1 = Node(1);
-	Node node_0 = Node(0);
-	Node node_2 = Node(2);
+	// Construct the BST
+	Node node_1 = Node(10);
 
 	BinarySearchTree bst = BinarySearchTree(node_1);
-	bst.insert(2);
+	bst.insert(3);
 	bst.insert(-12);
 	bst.insert(52);
 	bst.insert(19);
+	bst.insert(2);
+	bst.insert(-22);
+	bst.insert(43);
+	bst.insert(10);
+	bst.insert(123);
+	bst.insert(414);
+	bst.insert(-1000);
+	bst.insert(200);
 
-	// Expect: 1, -12, 2, 19, 52
+	// Test traversals
 	bst.visit_pre_order();
-	// Expect: -12, 1, 2, 19, 52
 	bst.visit_in_order();
 	bst.visit_post_order();
+	bst.depth_first_search();
 
+	// Unvisit all nodes
+	bst.unvisit_all_nodes();
+
+	bst.breadth_first_search();
+
+	// Test removal
 	bst.remove(2);
+	bst.remove(999);
+	bst.visit_in_order();
 	bst.remove(10);
 	bst.visit_in_order();
-	bst.remove(1);
+	bst.remove(10);
 	bst.visit_in_order();
 
 	return 0;
